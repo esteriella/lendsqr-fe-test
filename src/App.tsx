256d440a8dbe7835caa56581752d304d328bdeff
esteriella <damamy96@gmail.com>
2024-07-16 23:15:42 +0100

    Update
    Added react router dom dependency
    
    Added initial routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Login from './pages/login/Login';
 import './styles/App.scss';
+import {
+    Navigate,
+    Route,
+    Routes
+} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Login />
+            <Routes>
+                <Route path="/" element={<Navigate to='/login' />} />
+                <Route path="/login" element={<Login />} />
+                <Route>
+                    <>
+                    </>
+                </Route>
+            </Routes>
         </div>
     );
 }
